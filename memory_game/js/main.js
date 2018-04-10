7929abd1d15522e02207c8cbd4223323ab5ba583
Théodore Roos <fjodde98@gmail.com>
2018-04-10 22:37:04 +0200

    Started with JavaScript

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+var cardOne = "Queen of Diamonds";
+var cardTwo = "Queen of Hearts";
+var cardThree = "King of Diamonds";
+var cardFour = "King of Hearts";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
