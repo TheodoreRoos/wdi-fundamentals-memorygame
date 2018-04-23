81a0726a5cd5633ac8c1a325d4adec61b0babfb5
Théodore Roos <fjodde98@gmail.com>
2018-04-23 22:02:40 +0200

    Last assingment almost finished. Just couldn't flip the cards when clicked.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,6 +21,10 @@ var cards = [ {
 var cardsInPlay = [];
 
 var checkForMatch = function () {
+	// for (i = 0; i < cardsInPlay.length; i++) {
+	// 	var showCard = document.getElementById('game-board').children[i];
+	// 	showCard.setAttribute('src', showCard.getAttribute(cards.cardImage));
+	// }
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match");
@@ -31,19 +35,28 @@ var checkForMatch = function () {
 	}
 }
 
-var flipCard = function (cardID) {
-	console.log("User flipped " + cards[cardID].rank);
-	cardsInPlay.push(cards[cardID].rank);
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	console.log(cardId);
+	console.log("User flipped " + cards[cardId].rank);
+	// cardsInPlay.push(this.getAttribute('data-id'))
+	// console.log(cardsInPlay);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	checkForMatch();
-	console.log(cards[cardID].suit);
-	console.log(cards[cardID].cardImage);
 }
 
 var createBoard = function () {
 	for (i = 0; i < cards.length; i++) {
-		var newCard = document.createElement('img');
-		newCard.setAttribute('game-board', "images/back.png");
+		var backOfCard = document.createElement('img');
+		backOfCard.setAttribute('src', "images/back.png");
+		backOfCard.setAttribute('data-id', i);
+
+		backOfCard.addEventListener('click', flipCard);
+
+		document.getElementById('game-board').appendChild(backOfCard);
+	}
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
